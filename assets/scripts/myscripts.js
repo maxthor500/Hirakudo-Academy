41ab9e7eb6b1507f8f8cd177b7cebe3fae76cec4
maxthor500 <iraubuntu@gmail.com>
2021-02-04 19:30:54 +0000

    Add jQuery function to toggle class dark-mode throghout the elements and swap the images in myscripts.js

--- assets/scripts/myscripts.js
+++ assets/scripts/myscripts.js
@@ -6,4 +6,23 @@ $(document).ready(function() {
         $('.sidebar').toggleClass('expand');
     });
 
+    /* dark-mode */
+    $('.themify').click(function() {
+        let switchSelector = ['body', 'nav', 'a', '.checkbtn', '.navbar-nav', '.btn', 'h1', '.sidebar', 'img']
+
+        for (let i = 0; i < switchSelector.length; i++) {
+            $(switchSelector[i]).toggleClass('dark-mode');
+        }
+
+        /* swap images */
+        if ($('img').hasClass('dark-mode')) {
+            document.getElementById("logo").src = './assets/images/accademia-hirakudo-white.svg';
+            document.getElementById('hero-image').src = './assets/images/japan-white.svg';
+        } else {
+            document.getElementById("logo").src = './assets/images/accademia-hirakudo.svg';
+            document.getElementById('hero-image').src = './assets/images/japan.svg';
+        }
+
+    });
+
 });
