cdebf5ff76f93fadd2d201e6032d2369f392eac5
maxthor500 <iraubuntu@gmail.com>
2021-02-21 13:19:16 +0000

    add funtion to localstorage darkmode of the website to myscripts.js

--- assets/scripts/myscripts.js
+++ assets/scripts/myscripts.js
@@ -8,23 +8,19 @@ $(document).ready(function() {
 
     /* dark-mode */
     $('.themify').click(function() {
-        const switchSelector = ['body', 'nav', 'a', '.checkbtn', '.navbar-nav', '.btn', 'h1', '.sidebar', 'img', '.modal-content', '.close']
+        // Get the value of the "dark" item from the local storage on every click
+        setDarkMode = localStorage.getItem('dark-mode');
 
-        for (let i = 0; i < switchSelector.length; i++) {
-            $(switchSelector[i]).toggleClass('dark-mode');
-        }
-
-        /* swap images */
-        if ($('img').hasClass('dark-mode')) {
-            document.getElementById("logo").src = './assets/images/accademia-hirakudo-white.svg';
-            document.getElementById('hero-image').src = './assets/images/japan-white.svg';
+        if (setDarkMode !== "on") {
+            darkMode();
+            // Set the value of the item to "on" when dark mode is on
+            setDarkMode = localStorage.setItem('dark-mode', 'on');
         } else {
-            document.getElementById("logo").src = './assets/images/accademia-hirakudo.svg';
-            document.getElementById('hero-image').src = './assets/images/japan.svg';
-        };
-
+            darkMode();
+            // Set the value of the item to "null" when dark mode if off
+            setDarkMode = localStorage.setItem('dark-mode', null);
+        }
     });
-
 });
 
 function openModal(id) {
@@ -48,4 +44,29 @@ function closeModal(id, index) {
     let span = document.getElementsByClassName("close")[index];
     let modal = document.getElementById(id);
     modal.style.display = "none";
+}
+
+const darkMode = () => {
+    const switchSelector = ['body', 'nav', 'a', '.checkbtn', '.navbar-nav', '.btn', 'h1', '.sidebar', 'img', '.modal-content', '.close', '.calendar-container', '.calendar', '.container-fluid', '.memory-game', '.front-face', '#card-menu', '.card-game-info', '.info', '.overlay-text', '.send-btn'];
+    /* toggle dark-mode to every elements */
+    for (let i = 0; i < switchSelector.length; i++) {
+        $(switchSelector[i]).toggleClass('dark-mode');
+    }
+
+    /* swap images */
+    if ($('img').hasClass('dark-mode')) {
+        document.getElementById("logo").src = './assets/images/accademia-hirakudo-white.svg';
+        document.getElementById('hero-image').src = './assets/images/japan-white.svg';
+    } else {
+        document.getElementById("logo").src = './assets/images/accademia-hirakudo.svg';
+        document.getElementById('hero-image').src = './assets/images/japan.svg';
+    };
+}
+
+// Get the value of the "dark" item from the local storage
+let setDarkMode = localStorage.getItem('dark-mode');
+
+// Check dark mode is on or off on page reload
+if (setDarkMode === 'on') {
+    darkMode();
 }
